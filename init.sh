972a810b68c91328c2dcee2d09935b1221aa74ab
Luca Pasquale <luca.pasquale@moxoff.com>
2016-04-07 09:34:47 +0200

    Redirected output of cronjob to file so that it can be tailed by the docker command

--- init.sh
+++ init.sh
@@ -5,4 +5,4 @@ if [ ! -d /etc/letsencrypt/live ]; then
     ./letsencrypt-auto certonly --standalone --standalone-supported-challenges http-01 $@
 fi
 
-cron && tail -f /var/log/cron.log
+cron && tail -f /var/log/letsencrypt-cron.log

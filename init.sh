f91ecc676138c2ef8d9ed75d288b9365f8430729
Luca Pasquale <luca.pasquale@moxoff.com>
2018-05-08 11:18:12 +0200

    Make sure log file exists before tailing it

--- init.sh
+++ init.sh
@@ -5,4 +5,7 @@ if [ ! -d /etc/letsencrypt/live ]; then
     certbot certonly --standalone --preferred-challenges http --no-self-upgrade $@
 fi
 
+mkdir -p /var/log/letsencrypt/
+touch /var/log/letsencrypt/letsencrypt.log
+
 cron && tail -f /var/log/letsencrypt/letsencrypt.log

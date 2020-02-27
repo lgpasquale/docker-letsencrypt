64e6be2db7c9ab23f20be05bd1d32676b10ad90b
Luca Pasquale <luca.pasquale@moxoff.com>
2020-02-27 08:41:38 +0100

    Always call certbot when the container starts (to create new certs if needed)

--- init.sh
+++ init.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 cd /
-if [ ! -d /etc/letsencrypt/live ]; then
-    until certbot certonly --standalone --preferred-challenges http --no-self-upgrade -n --agree-tos $@
-    do
-        echo "Retrying in 60 seconds..."
-        sleep 60
-    done
-fi
+until certbot certonly --standalone --preferred-challenges http --no-self-upgrade -n --agree-tos $@
+do
+    echo "Retrying in 60 seconds..."
+    sleep 60
+done
 
 mkdir -p /var/log/letsencrypt/
 touch /var/log/letsencrypt/letsencrypt.log
